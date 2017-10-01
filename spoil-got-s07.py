6d6ee134c98135a14cd1ee61f6e07c519cca36a6
zak39 <fotia.baptiste@hotmail.com>
2017-10-01 16:54:28 +0200

    Ajout des print dans spoil-got-s07.py

---
+++ spoil-got-s07.py
@@ -0,0 +1,7 @@
+# -*- coding: latin-1 -*-
+
+print("À la fin de la saison 7 de Game Of Thrones, le Roi de la Nuit et l'armée des morts passent le mur avec Viserion après sa résurrection.")
+print("...")
+print("...")
+print("...")
+print("J'ai réussi l'exercice Git-GitHub ou pas ?")
